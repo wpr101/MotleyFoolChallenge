fecb2897b271e871c47e36a5cbe9d7355220abb1
Rampage <wpr101@gmail.com>
2018-04-21 11:56:33 -0400

    view setup

--- fool/stocks/views.py
+++ fool/stocks/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
-
-# Create your views here.
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
-
+from django.template import loader
+import json
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    with open("./stocks/quotes_data.json", encoding='utf-8') as json_data:
+        data = json.load(json_data)
+
+    #return HttpResponse(template.render('index.html'))
+    return render(request, 'stocks/index.html', {})
+    #return render_to_response('fool/index.html')
